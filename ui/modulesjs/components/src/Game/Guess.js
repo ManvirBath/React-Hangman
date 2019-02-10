752487a639932a6b4242b5805c02971b3d614abf
Mano Toor <manotoor@gmail.com>
2019-02-09 21:22:20 -0800

    Teaching money about branches

--- ui/modulesjs/components/src/Game/Guess.js
+++ ui/modulesjs/components/src/Game/Guess.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Guess extends Component{
   render(){
     return (
-      <div> Hello </div>
+      <div> {this.props.answer}</div>
     );
   }
 }
